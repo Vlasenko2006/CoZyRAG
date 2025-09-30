d774f7e3c235a7551a3982e3edd65ebd939b2e63
Andrey <74958459+Vlasenko2006@users.noreply.github.com>
2025-09-30 22:28:31 +0200

    Update exec.bash

--- exec.bash
+++ exec.bash
@@ -18,15 +18,6 @@ MASTER_PORT=$((10000 + RANDOM % 50000))
 module load git
 export PATH=/sw/spack-levante/git-2.43.7-2ofazl/bin:$PATH
 
-# Build index (serial, single rank)
-echo "Building FAISS index..."
-# /work/gg0302/g260141/RAG/rag_env/bin/python3.10 RAG.py --build_index
-if [ $? -ne 0 ]; then
-  echo "Index building failed! Aborting job."
-  exit 1
-fi
-echo "Building indexes completed ==="
-
 # Export variables for distributed run
 export MASTER_ADDR MASTER_PORT
 
