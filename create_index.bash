6ed62cd06aaea0821799d3cfabc2a108a9247936
Andrey <74958459+Vlasenko2006@users.noreply.github.com>
2025-09-30 22:32:07 +0200

    Create create_index.bash

---
+++ create_index.bash
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+#SBATCH --job-name=rag
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=4
+#SBATCH --gpus-per-node=4
+#SBATCH --time=07:59:00
+#SBATCH --account=your account
+#SBATCH --partition=gpu
+#SBATCH --error=e-index.out
+#SBATCH --output=index.out
+#SBATCH --exclusive
+#SBATCH --mem=0
+
+MASTER_ADDR=$(scontrol show hostnames $SLURM_JOB_NODELIST | head -n 1)
+MASTER_PORT=$((10000 + RANDOM % 50000))
+
+module load git
+export PATH=/sw/spack-levante/git-2.43.7-2ofazl/bin:$PATH
+
+# Build index (serial, single rank)
+echo "Building FAISS index..."
+/path to your environment/python3.10 RAG.py --build_index
+if [ $? -ne 0 ]; then
+  echo "Index building failed! Aborting job."
+  exit 1
+fi
+echo "Building indexes completed ==="
